f856f3fde906df1cf37e419c0ba16c18b1f6c94e
amilyk <amily_k@163.com>
2017-02-13 23:40:42 +0800

    python baike.baidu

---
+++ html_outputer.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+
+class HtmlOutputer(object):
+    def __init__(self):
+        self.datas = []
+
+    def collect_data(self,data):
+        if data is None:
+            return
+        self.datas.append(data)
+
+    #ascii python默认编码，所以转成网页编码utf8
+    def output_html(self):
+        #新建html
+
+        fout = open('output.html','w')
+        #写入数据
+        fout.write("<html>")
+        fout.write("<body>")
+        fout.write("<table>")
+
+        for data in self.datas:
+            fout.write("<tr>")
+            fout.write("<td>%s</td>" % data['url'])
+            fout.write("<td>%s</td>" % data['title'].encode('utf-8'))
+            fout.write("<td>%s</td>" % data['summary'].encode('utf-8'))
+            fout.write("</tr>")
+
+        fout.write("</table>")
+        fout.write("</body>")
+        fout.write("</html>")
+
+        fout.close()
+
+
---
+++ spider_main.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##加utf格式就能支持中文注释
+
+# 1 调度器
+# 入口URL作为参数爬取页面
+from spider import url_manager, html_downloader, html_parser, html_outputer
+
+
+class SpiderMain(object):  # 创建类快捷键ctrl+1
+    def __init__(self):
+        self.urls = url_manager.UrlManager()  # url管理器
+        self.downloader = html_downloader.HtmlDownloader()  # url下载器
+        self.parser = html_parser.HtmlParser()  # url解析器
+        self.outputer = html_outputer.HtmlOutputer()  # html输出器
+
+    def craw(self, root_url):  # 入口url添加进管理器
+        cnt = 1
+        self.urls.add_new_url(root_url)  # 当url管理器有待爬取的url，启动循环
+        while self.urls.has_new_url():
+            try:
+                new_url = self.urls.get_new_url()  # 获取一个待爬取的
+                print 'craw %d: %s' % (cnt, new_url)  ##爬取的为第几个url
+                html_cont = self.downloader.download(new_url)  # url下载器
+                new_urls, new_data = self.parser.parse(new_url, html_cont)  # 当前爬取的新url，解析后的价值数据
+                self.urls.add_new_urls(new_urls)
+                self.outputer.collect_data(new_data)
+
+                if cnt == 1000:
+                    break
+                cnt = cnt + 1
+
+            except:
+                print 'craw failed'
+
+        self.outputer.output_html()
+
+
+if __name__ == "__main__":
+    # python词条 入口URL
+    root_url = "http://baike.baidu.com/view/21087.htm"
+    obj_spider = SpiderMain()
+    # 启动爬虫
+    obj_spider.craw(root_url)
